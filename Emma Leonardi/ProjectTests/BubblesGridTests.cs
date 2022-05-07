1f62f9397416658ff4817f514a176df9568648ed
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-05-07 11:47:27 +0200

    Created tests for Bubble, TripletIntegerUtility and BubblesGrid

---
+++ Emma Leonardi/ProjectTests/BubblesGridTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Tests
+{
+    [TestClass()]
+    public class BubblesGridTests
+    {
+        [TestMethod()]
+        public void BubblesGridTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BubblesGridTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetBubblesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLastRowYTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EndReachedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddBubbleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveBubbleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsBubbleAttachableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoveBubblesDownTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckForUnconnectedBubblesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSameColorNeighborsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveBubblesCascadingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveUnconnectedBubblesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
