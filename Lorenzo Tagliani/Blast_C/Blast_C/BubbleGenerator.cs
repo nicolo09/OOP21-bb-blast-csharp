cd8cad4d14ad86a2351c18e6e379b94fb016e06e
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-05-07 16:01:06 +0200

    Added BubbleGenerator interface and class c#

---
+++ Lorenzo Tagliani/Blast_C/Blast_C/BubbleGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Project;
+
+namespace Blast_C
+{
+    /// <summary>
+    /// Class for the generation of a random bubble.
+    /// </summary>
+    class BubbleGenerator : IBubbleGenerator
+    {
+        private readonly List<COLOR> _col;
+        private readonly Random _rnd;
+        /// <summary>
+        /// Constructor to generate list of color and random.
+        /// </summary>
+        /// <param name="col">List of colors.</param>
+        public BubbleGenerator(List<COLOR> col)
+        {
+            _col = col;
+            _rnd = new Random();
+        }
+        /// <summary>
+        /// Generate a bubble.
+        /// </summary>
+        /// <param name="p">Position of the bubble to generate</param>
+        /// <returns>Random color generated bubble.</returns>
+        public Bubble Generate(Tuple<double, double> p)
+        {
+            Array v = Enum.GetValues(typeof(COLOR));
+            if (_col.Count == 0)
+            {
+                return new Bubble(p, COLOR.PURPLE);
+            }
+            COLOR color = (COLOR)v.GetValue(_rnd.Next(v.Length-1));
+            return new Bubble(p, color);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is BubbleGenerator generator &&
+                   EqualityComparer<List<COLOR>>.Default.Equals(_col, generator._col) &&
+                   EqualityComparer<Random>.Default.Equals(_rnd, generator._rnd);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_col, _rnd);
+        }
+    }
+}
---
+++ Lorenzo Tagliani/Blast_C/Blast_C/IBubbleGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Project;
+
+namespace Blast_C
+{
+    /// <summary>
+    /// Interface for the bubble generator.
+    /// </summary>
+    interface IBubbleGenerator
+    {
+        /// <summary>
+        /// Generate a bubble.
+        /// </summary>
+        /// <param name="p">Position of the bubble.</param>
+        /// <returns>The generated bubble.</returns>
+        Bubble Generate(Tuple<double, double> p);
+    }
+}
