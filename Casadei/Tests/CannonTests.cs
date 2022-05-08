250966c99e87869a047a72f55180c06e1bdbd7fc
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-05-08 16:15:37 +0200

    Created test class for Cannon

---
+++ Casadei/Tests/CannonTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Blast_C;
+
+namespace Project.Tests
+{
+    [TestClass]
+    public class CannonTests
+    {
+        [TestMethod]
+        public void MoveCannonTest()
+        {
+            List<COLOR> colors = new List<COLOR>((COLOR[])Enum.GetValues(typeof(COLOR)));
+            IBubbleGenerator generator = new BubbleGenerator(colors);
+            ICannon cannon = new Cannon(new Tuple<double, double>(0, 0), 60, 10, generator);
+            int angle = 145;
+
+            cannon.Move(angle);
+            Assert.AreEqual(angle, cannon.Angle);
+
+            angle = 176;
+            cannon.Move(angle);
+            Assert.AreNotEqual(angle, cannon.Angle);
+
+            angle = 4;
+            cannon.Move(angle);
+            Assert.AreNotEqual(angle, cannon.Angle);
+        }
+
+        [TestMethod]
+        public void GetCurrentlyLoadedBubbleTest()
+        {
+            List<COLOR> colors = new List<COLOR>((COLOR[])Enum.GetValues(typeof(COLOR)));
+            IBubbleGenerator generator = new BubbleGenerator(colors);
+            ICannon cannon = new Cannon(new Tuple<double, double>(0, 0), 60, 10, generator);
+
+            var loaded = cannon.GetCurrentlyLoadedBubble();
+            var shot = cannon.Shoot();
+
+            Assert.AreEqual(loaded, shot, "The initially loaded bubble and the shot are the same");
+        }
+    }
+}
