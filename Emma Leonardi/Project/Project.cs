c3668d0679dfde6f2054e435513cb84bbd1e68f0
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-05-08 21:00:45 +0200

    Created main

---
+++ Emma Leonardi/Project/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Project
+    {
+        static void Main()
+        {
+            Console.WriteLine("Progetto di OOP21-bb-blast-csharp, codice di Emma Leonardi");
+
+        }
+    }
+}
